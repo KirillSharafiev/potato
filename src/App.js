d797c57fcdb07a47174a2deed207e7b84db6fe1a
Kirill <sharafiev-k@yandex.ru>
2024-07-05 16:25:00 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './scss/main.scss'
+import NavBar from './blocks/NavBar';
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
